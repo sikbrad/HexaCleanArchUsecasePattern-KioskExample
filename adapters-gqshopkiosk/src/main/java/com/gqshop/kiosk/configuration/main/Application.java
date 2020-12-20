4643b88398a9a9052eb5414552c9512c86c5ffcf
sikbrad <sikbrad@gmail.com>
2020-12-20 21:39:11 +0900

    made structure

---
+++ adapters-gqshopkiosk/src/main/java/com/gqshop/kiosk/configuration/main/Application.java
@@ -0,0 +1,27 @@
+package com.gqshop.kiosk.configuration.main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			logger.info("Main bean created");
+		};
+	}
+
+}
